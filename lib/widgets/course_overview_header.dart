b11e45b97a30047135646f653c2f9d5ea61cba12
luisghdez <1gluishernandez@gmail.com.com>
2025-02-26 16:51:06 -0700

    course overview header fix

--- lib/widgets/course_overview_header.dart
+++ lib/widgets/course_overview_header.dart
@@ -15,46 +15,47 @@ class CourseOverviewHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      // Rounds all corners
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
-        child: Container(
-          // Add padding to account for status bar and provide spacing.
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(140, 0, 0, 0),
-            borderRadius: const BorderRadius.all(Radius.circular(30)),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.2), // Light border color.
-              width: 1,
-            ),
-          ),
-          child: Row(
-            children: [
-              // Back button
-              IconButton(
-                icon: const Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: Color.fromARGB(131, 255, 255, 255),
-                ),
-                onPressed: () {
-                  onBack();
-                },
+    return SizedBox(
+      height: 60,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(30)),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(140, 0, 0, 0),
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.2),
+                width: 1,
               ),
-              const SizedBox(width: 16),
-              // Lesson title and bold "Math Galaxy"
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 14.0, horizontal: 14.0),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                // Back button positioned to the left
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    iconSize: 20,
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: Color.fromARGB(131, 255, 255, 255),
+                    ),
+                    onPressed: () => onBack(),
+                  ),
+                ),
+                // Centered text
+                Center(
                   child: RichText(
                     textAlign: TextAlign.center,
                     text: TextSpan(
                       text: lessonTitle,
                       style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w100),
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w100,
+                      ),
                       children: const [
                         TextSpan(
                           text: ' Galaxy',
@@ -66,8 +67,8 @@ class CourseOverviewHeader extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
