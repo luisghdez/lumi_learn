dbb0d636d8d4767e358205346f3fde3b56df187f
luisghdez <1gluishernandez@gmail.com.com>
2025-03-30 18:24:19 -0600

    keyboard resize fix

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -9,9 +9,6 @@ import 'package:lumi_learn_app/screens/social/friends_screen.dart';
 import 'package:lumi_learn_app/widgets/bottom_nav_bar.dart';
 import 'package:lumi_learn_app/screens/leaderboard/leaderboard_screen.dart';
 
-
-
-
 class MainScreen extends StatelessWidget {
   MainScreen({Key? key}) : super(key: key);
 
@@ -20,6 +17,8 @@ class MainScreen extends StatelessWidget {
     final NavigationController navigationController = Get.find();
 
     return Scaffold(
+      resizeToAvoidBottomInset:
+          false, // Prevents resizing when the keyboard appears
       body: Stack(
         children: [
           // Listen to scrolling for hiding/showing the navbar
