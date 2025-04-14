7e1ca578e8f44c1c1274d52b1e760c51f17470d3
luisghdez <1gluishernandez@gmail.com.com>
2025-04-14 12:24:12 -0600

    speak screen for ipad

--- lib/screens/courses/lessons/widgets/lesson_progress_bar.dart
+++ lib/screens/courses/lessons/widgets/lesson_progress_bar.dart
@@ -18,26 +18,26 @@ class LessonProgressBar extends StatelessWidget {
     Get.dialog(
       Align(
         alignment: Alignment.bottomCenter,
-        child: Container(
-          margin: const EdgeInsets.all(12),
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 12, 12, 12),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(color: greyBorder, width: 1),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/astronaut/crying.png',
-                height: 130,
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: 0.7 * Get.width,
-                child: const Text(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 700),
+          child: Container(
+            margin: const EdgeInsets.all(12),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 12, 12, 12),
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: greyBorder, width: 1),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/astronaut/crying.png',
+                  height: 130,
+                ),
+                const SizedBox(height: 16),
+                const Text(
                   'If you quit now, all your stars from this lesson will disappear!',
                   style: TextStyle(
                     fontWeight: FontWeight.w500,
@@ -48,46 +48,46 @@ class LessonProgressBar extends StatelessWidget {
                   textAlign: TextAlign.center,
                   softWrap: true,
                 ),
-              ),
-              const SizedBox(height: 28),
-              Column(
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () => Get.back(),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        padding: const EdgeInsets.symmetric(vertical: 12),
+                const SizedBox(height: 28),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () => Get.back(),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Keep Learning',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.red.shade600,
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                          borderRadius: BorderRadius.circular(6),
                         ),
                       ),
-                      child: const Text(
-                        'Keep Learning',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
+                      onPressed: () {
+                        Get.back(); // close dialog
+                        Get.back(); // exit lesson
+                      },
+                      child: const Text('Quit', style: TextStyle(fontSize: 16)),
                     ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.red.shade600,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                    ),
-                    onPressed: () {
-                      Get.back(); // close dialog
-                      Get.back(); // exit lesson
-                    },
-                    child: const Text('Quit', style: TextStyle(fontSize: 16)),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
