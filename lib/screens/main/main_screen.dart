493cde6bd75c880e6c2f57b8eefb09f2d3d2905c
luisghdez <1gluishernandez@gmail.com>
2026-05-01 18:17:51 -0600

    now complete

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:lumi_learn_app/application/controllers/navigation_controller.dart';
 import 'package:lumi_learn_app/screens/feed/feed_screen.dart';
@@ -15,35 +14,19 @@ class MainScreen extends StatelessWidget {
     final NavigationController navigationController = Get.find();
 
     return Scaffold(
-      resizeToAvoidBottomInset:
-          false, // Prevents resizing when the keyboard appears
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
-          // Listen to scrolling for hiding/showing the navbar
-          NotificationListener<ScrollNotification>(
-            onNotification: (notification) {
-              if (notification is UserScrollNotification) {
-                if (notification.direction == ScrollDirection.forward) {
-                  navigationController.showNavBar();
-                } else if (notification.direction == ScrollDirection.reverse) {
-                  navigationController.hideNavBar();
-                }
-              }
-              return false;
-            },
-            child: Obx(
-              () => IndexedStack(
-                index: navigationController.currentIndex.value,
-                children: const [
-                  FeedScreen(),
-                  HomeScreen(),
-                  ProfileScreen(),
-                ],
-              ),
+          Obx(
+            () => IndexedStack(
+              index: navigationController.currentIndex.value,
+              children: const [
+                FeedScreen(),
+                HomeScreen(),
+                ProfileScreen(),
+              ],
             ),
           ),
-
-          // Floating Bottom Navbar
           const Align(
             alignment: Alignment.bottomCenter,
             child: BottomNavbar(),
